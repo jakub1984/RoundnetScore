1e59109dd0da75b1ba0051ab3be2936307f4486b
Jakub Perich <j.perich@seznam.cz>
2019-12-27 13:50:20 +0100

    Add result screen

---
+++ RoundnetScore/ResultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultViewController.swift
+//  RoundnetScore
+//
+//  Created by Jakub Perich on 27/12/2019.
+//  Copyright © 2019 com.jakubperich. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var resultLbl: UILabel!
+    
+}
